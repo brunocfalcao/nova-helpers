15fe3e344438e52e32e69bb7e58f5cdda702ab42
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-06-14 13:38:35 +0200

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,32 @@
 <?php
+
+use Illuminate\Support\Arr;
+
+function nova_current_view_context()
+{
+    $namespace = Arr::first(explode('@', Route::getCurrentRoute()->getAction()['controller']));
+
+    $controller = Arr::last(explode('\\', $namespace));
+
+    switch ($controller) {
+        case "ResourceIndexController":
+            return 'index';
+            break;
+
+        case "ResourceShowController":
+            return 'show';
+            break;
+
+        case "ResourceDetailController":
+            return 'detail';
+            break;
+
+        case "UpdateFieldController":
+            return 'update';
+            break;
+
+        case "CreationFieldController":
+            return 'create';
+            break;
+    }
+}
